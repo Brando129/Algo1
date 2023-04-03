42f5466d8b80575799bdaeb6285afa03adecc267
Brandon Colangelo <brandoncancode@gmail.com>
2023-04-03 08:44:08 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// A function that will return all the numbers from zero to the input given
+// by a user
+
+function allIntegers(num) {
+var numbers = []
+
+    for (var i = 0; i < num; i++) {
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+allIntegers()
